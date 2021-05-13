a3d42d60e3207a7c92ad5c7493334f94292e0340
Sergiy <44747525+Tsurkanovskiy@users.noreply.github.com>
2021-05-13 14:33:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from pymongo import MongoClient
-#import psycopg2
 import random
 import time
 #from config import config
-#from sqlcommands import sqlcreate, sqldrop, sqlselect, sqlinsert
 
 
 from pprint import pprint
@@ -20,16 +18,18 @@ def clear_sides(line):
 	return line
 
 def import_to_db(year, db, test_fall_chance = 0):
+	count_lines = len(open('Odata' + year + 'File.csv').readlines(  ))
+	print(count_lines)
 	with open("log.txt") as log_file:
 		log = log_file.readline()
 		if (log == ""):
-			cluster_num = 0
+			line_num = 0
 		else:
 			line = log.split(";")
 			if (int(year)<int(line[1])):
 				return 1
 			else:
-				cluster_num = int(line[0])
+				line_num = int(line[0])
 	duration = float(time.time())
 	with open('Odata' + year + 'File.csv') as csvfile:
 		header = csvfile.readline()
@@ -39,6 +39,8 @@ def import_to_db(year, db, test_fall_chance = 0):
 		header.append("year")
 
 		n = 0
+		while n < line_num:
+			csvfile.readline()
 		for line in csvfile:
 			arg_lst = []
 			line = line.split(";")
@@ -55,35 +57,29 @@ def import_to_db(year, db, test_fall_chance = 0):
 			arg_lst.append(year)
 			
 			participant_info = dict(zip(header, arg_lst))
-			result=db.participant_info.insert_one(participant_info)
-			log_file = open("log.txt", "w")
-			log_file.write(str(cluster_num) + ";" + year)
-			log_file.close()
 			n+=1
+			log_file = open("log.txt", "w")
+			log_file.write(str(n) + ";" + year)
+			log_file.close()
+			
+			
+
+			try:
+				result = db.participant_info.insert_one(participant_info)
+			#except psycopg2.InterfaceError as e:
+			'''except:
+				log_file = open("log.txt", "w")
+				log_file.write(str(n - 1) + ";" + year)
+				log_file.close()
+				#raise e'''
 			if(n==1000):
 			    break
-			'''if ((n > 0)&((n % 50) == 0)):
-				try:
-					# Коміт (Не буде потрібний)
-					conn.commit()
-				except psycopg2.InterfaceError as e:
-					log_file = open("log.txt", "w")
-					log_file.write(str(cluster_num - 1) + ";" + year)
-					log_file.close()
-					raise e
-				cluster_num+=1
-				log_file = open("log.txt", "w")
-				log_file.write(str(cluster_num) + ";" + year)
-				log_file.close()
 			if (random.randint(0, test_fall_chance) == 1):
 				print("Потеряно соединение с базой данных")
-				# Розірвання зв’язку
-				conn.close()
-		# Коміт (Не буде потрібний)
-		conn.commit()
+				db.close()
 		duration = round((float(time.time()) - duration), 4)
 		with open('upload_time.txt','a') as upload_time:
-			upload_time.write('Data from Odata' + year + 'File.csv uploaded in ' + str(duration) + ' seconds\n')'''
+			upload_time.write('Data from Odata' + year + 'File.csv uploaded in ' + str(duration) + ' seconds\n')
 	return 0	
 
 
@@ -99,25 +95,20 @@ except FileNotFoundError:
 
 years = ['2019', '2020']
 
-'''test_fall = input("Желеете протестировать сценарий 'падения' базы данных? (y/n)")
+test_fall = input("Желеете протестировать сценарий 'падения' базы данных? (y/n)")
 if (test_fall == "y"):
 	test_fall_chance = int(input("Пожалуйста введите n (вероятность падения базы данных после анализа строчки - 1/n): "))
 else:
-	test_fall_chance = 0'''
+	test_fall_chance = 0
 client = MongoClient("mongodb://localhost:27017")
 db=client.ZNO_data
-# Перевірка на існування таблиці
-'''cur.execute("select exists(select * from information_schema.tables where table_name='hist_results')")
-if (cur.fetchone()[0]):
-	drop = input("Желеете удалить базу данных? (y/n)")
-	if (drop == "y"):'''
-		# Видалення таблиці
-coll = db.participant_info
-coll.drop()
-# Створення таблиці
-'''cur.execute("select exists(select * from information_schema.tables where table_name='hist_results')")
-if (not (cur.fetchone()[0])):
-	cur = sqlcreate(cur)'''
+
+
+drop = input("Желеете удалить базу данных? (y/n)")
+if (drop == "y"):
+	coll = db.participant_info
+	coll.drop()
+
 
 for year in years:
 	import_to_db(year, db)
@@ -130,6 +121,7 @@ regions = db.participant_info.aggregate([
 	{ "$group": { '_id': "$physPTRegName"} }
 ])
 
+print(db.participant_info.find().count())
 
 regions = list(regions)
 for i in range(len(regions)):
@@ -145,11 +137,9 @@ for year in years:
 	   { "$group": { '_id': "$physPTRegName", "ball": { "$avg": "$physBall100" } } }
 	])
 	phys_avg = list(phys_avg)
-	print(phys_avg)
 	for doc in phys_avg:
 		print(doc['ball'])
-		phys_avg_result[doc['_id']].append(str(doc['ball']))
-	print(phys_avg_result)
+		phys_avg_result[doc['_id']].append(str(round(doc['ball'], 3)))
 
 
 
@@ -161,10 +151,9 @@ for year in years:
 
 
 csv_data = [(key + ";" + ";".join(value)) for key, value in phys_avg_result.items()]
-pprint(csv_data)
 '''
 with open("Result.csv", "w") as result_file:
-	result_file.write("Регіон;Найнижчий бал в 2019;Найнижчий бал в 2020")
+	result_file.write("Регіон;" + ";".join([("Середній бал по фізиці в " + year + " році") for year in years]))
 	for item in csv_data:
 		result_file.write("\n")
 		result_file.write(item)
